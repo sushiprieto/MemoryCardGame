b5cfcd1b3dc9ddcf2d7351b04010bc63277de640
Carlos Prieto Pareja <cprieto@linux-107-13.info2>
2017-01-24 10:11:36 +0100

    Añadida mejora pantalla carga

--- app/src/main/java/com/iesnervion/cprieto/memorycardgame/PantallaInicioActivity.java
+++ app/src/main/java/com/iesnervion/cprieto/memorycardgame/PantallaInicioActivity.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class PantallaInicioActivity extends AppCompatActivity {
+
+    //ProgressBar carga;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_pantalla_inicio);
+
+        //carga = (ProgressBar)findViewById(R.id.proCarga);
+        //carga.setVisibility(carga.VISIBLE);
 
         new Handler().postDelayed(new Runnable() {
             @Override
